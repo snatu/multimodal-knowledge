f0e1297078833a6b3c96777f2ef4d3f4478d6e15
sherylm77 <sherylmathew77@gmail.com>
2022-07-28 22:47:07 +0000

    clean up make_json, add original prep_data bash script

--- make_json.py
+++ make_json.py
@@ -35,11 +35,11 @@ def make_json_for(vids, file_name):
                 question = ':'.join(str_list).strip()
                 vid_dict['q'] = question
                 vid_dict["qid"] = vid + "_" + question_num
-                if vid in ["C08WmKiwcSs", "n5_HdNzf03Q", "y1Y02_oZP8U", "SjrMprYa608", "D2g3gTRkv0U"]:
-                    continue
-                if vid_dict["qid"] in ["58DqoE56OWc_q2", "T8JwNZBJ_wI_q7", "2XFVnzr4Vho_q1", "eDqEcrIRxgQ_q2", "eDqEcrIRxgQ_q3", "eDqEcrIRxgQ_q4", "eDqEcrIRxgQ_q5", "Jp_KHLvQcuw_q3", "Jp_KHLvQcuw_q6", "Jp_KHLvQcuw_q11", "bMuoPr5-Yt4_q3", "gbVOyKifrAo_q1", "gbVOyKifrAo_q2", "gbVOyKifrAo_q6", "srWtQnseRyE_q1", "srWtQnseRyE_q2", "srWtQnseRyE_q3", "q45sJ2n2XPg_q1", "q45sJ2n2XPg_q5", "q45sJ2n2XPg_q6", "_UJNNySGM6Q_q2", "_UJNNySGM6Q_q3", "_UJNNySGM6Q_q4", "VP4rHzYyuL0_q2", "VP4rHzYyuL0_q4", "VP4rHzYyuL0_q6", "N-6zVmVuTs0_q2", "N-6zVmVuTs0_q6", "N-6zVmVuTs0_q7",
-                                        "gBs-CkxGXy8_q2", "gBs-CkxGXy8_q3", "gBs-CkxGXy8_q6", "gBs-CkxGXy8_q7", "gBs-CkxGXy8_q10", "gBs-CkxGXy8_q11", "E4MUXs4IHtY_q4", "ZuYTtKZUkJc_q1", "ZuYTtKZUkJc_q2", "j1CTHVQ8Z3k_q1", "j1CTHVQ8Z3k_q3", "j1CTHVQ8Z3k_q4", "j1CTHVQ8Z3k_q5", "j1CTHVQ8Z3k_q6", "j1CTHVQ8Z3k_q7", "erOpqmubBL4_q1", "H0Qdz8bSkv0_q2", "H0Qdz8bSkv0_q3", "H0Qdz8bSkv0_q4", "mpHoYhIFKNI_q1", "aqGNOsZFdBU_q1", "aqGNOsZFdBU_q5", "FositxHjuUk_q1"]:
-                    continue
+                # if vid in ["C08WmKiwcSs", "n5_HdNzf03Q", "y1Y02_oZP8U", "SjrMprYa608", "D2g3gTRkv0U"]:
+                #     continue
+                # if vid_dict["qid"] in ["58DqoE56OWc_q2", "T8JwNZBJ_wI_q7", "2XFVnzr4Vho_q1", "eDqEcrIRxgQ_q2", "eDqEcrIRxgQ_q3", "eDqEcrIRxgQ_q4", "eDqEcrIRxgQ_q5", "Jp_KHLvQcuw_q3", "Jp_KHLvQcuw_q6", "Jp_KHLvQcuw_q11", "bMuoPr5-Yt4_q3", "gbVOyKifrAo_q1", "gbVOyKifrAo_q2", "gbVOyKifrAo_q6", "srWtQnseRyE_q1", "srWtQnseRyE_q2", "srWtQnseRyE_q3", "q45sJ2n2XPg_q1", "q45sJ2n2XPg_q5", "q45sJ2n2XPg_q6", "_UJNNySGM6Q_q2", "_UJNNySGM6Q_q3", "_UJNNySGM6Q_q4", "VP4rHzYyuL0_q2", "VP4rHzYyuL0_q4", "VP4rHzYyuL0_q6", "N-6zVmVuTs0_q2", "N-6zVmVuTs0_q6", "N-6zVmVuTs0_q7",
+                #                         "gBs-CkxGXy8_q2", "gBs-CkxGXy8_q3", "gBs-CkxGXy8_q6", "gBs-CkxGXy8_q7", "gBs-CkxGXy8_q10", "gBs-CkxGXy8_q11", "E4MUXs4IHtY_q4", "ZuYTtKZUkJc_q1", "ZuYTtKZUkJc_q2", "j1CTHVQ8Z3k_q1", "j1CTHVQ8Z3k_q3", "j1CTHVQ8Z3k_q4", "j1CTHVQ8Z3k_q5", "j1CTHVQ8Z3k_q6", "j1CTHVQ8Z3k_q7", "erOpqmubBL4_q1", "H0Qdz8bSkv0_q2", "H0Qdz8bSkv0_q3", "H0Qdz8bSkv0_q4", "mpHoYhIFKNI_q1", "aqGNOsZFdBU_q1", "aqGNOsZFdBU_q5", "FositxHjuUk_q1"]:
+                #     continue
                 answer_num = 0
                 correct_num = 0
                 incorrect_num = 0
@@ -70,10 +70,10 @@ def make_json_for(vids, file_name):
                             all_incorrect_ans.append(answer)
                     else:
                         # question
-                        if len(all_incorrect_ans) != 3:
-                            print(vid_dict['qid'])
-                            vid_file.seek(pos)
-                            break
+                        # if len(all_incorrect_ans) != 3:
+                        #     print(vid_dict['qid'])
+                            # vid_file.seek(pos)
+                            # break
                         # for ans in all_correct_ans:
                         #     all_answers = []
                         #     all_answers.extend(all_incorrect_ans)
